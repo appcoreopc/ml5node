7121a505df2dd1cf04fdfd0a2d59252903241777
appcoreopc <jeremy@jeremys-MacBook-Pro.local>
2021-04-04 04:12:16 +0800

    update model

--- src/index.ts
+++ src/index.ts
@@ -11,17 +11,16 @@ async function run() {
     // Generate some synthetic data for training. (y = 2x - 1)
     const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
     const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
-
+ 
     // Train the model using the data.
     await model.fit(xs, ys, { epochs: 250 });
 
     // Use the model to do inference on a data point the model hasn't seen.
     // Should print approximately 39.
     //document.getElementById('micro-out-div').innerText =
-    const prediction = model.predict(tf.tensor2d([20], [1, 1]));
+    const prediction = model.predict(tf.tensor2d([20, 21], [2, 1]));
 
     console.log(prediction);
-
 }
 
 run();
